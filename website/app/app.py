dd34bcb93c39947f7ef6e3706a0b9c47738288a3
PeggyZWY <510720619@qq.com>
2019-06-10 20:32:27 +1000

    ADD: cartogram; MOD: automation

--- website/app/app.py
+++ website/app/app.py
@@ -1,11 +1,28 @@
+# -*- coding: utf-8 -*-
+
 from flask import Flask, jsonify, render_template, url_for
 
 import requests, json
 
 app = Flask(__name__)
 
+first_country = 'Afghanistan'
+last_country = 'Yemen'
+
+states_list = ['ACT', 'NSW', 'NT', 'QLD', 'SA', 'TAS', 'VIC', 'WA'] # must keep this order
+
+sa2_list = []
+
+with open('static/map/sa2_list_dict.js') as f:
+    content = f.read()
+    content = json.loads(content)
+    length = len(content)
+    for i in range(length):
+        sa2_list.append(content[str(i)])
+
+
 headers = {
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/json',
     'Accept-Encoding': 'gzip, deflate, br',
     'Accept-Language': 'en,zh-CN;q=0.9,zh;q=0.8',
     'Authorization': 'Basic dXNlcjpDNGtlRnIxZDR5NQ==',
@@ -19,16 +36,176 @@ headers = {
 def index():
     return render_template('index.html')
 
+@app.route('/estimation')
+def estimation():
+    return render_template('estimation.html')
 
-@app.route('/getjson')
-def getjson():
-    # data = {'name': 'cats', 'attribute': 'cute'}
-    # data = json.dumps(data)
-    # return data
-    response = requests.get('http://103.6.254.104/getjsonData', headers=headers, verify=False)
-    # data_json = json.loads(response.text)
-    # return data_json
-    return response.text
+@app.route('/test')
+def test():
+    return render_template('test.html')
+
+@app.route('/cartogram')
+def cartogram():
+    return render_template('cartogram.html')
+
+# @app.route('/cartogram_with_params')
+# def cartogram_with_params():
+#     return render_template('cartogram_with_params.html', year=)
+
+
+@app.route('/get_VIC_topo')
+def get_VIC_topo():
+    return render_template('test_VIC.html')
+
+# http://127.0.0.1/getjson/country/all/country/all
+# /getjson/country/all/country/all
+@app.route('/getjson/<ori_category>/<ori_name>/<au_category>/<au_name>')
+def getjson(ori_category, ori_name, au_category, au_name):
+    # database = 'test'
+    # database = 'data_01_06'
+    # database = 'data_01_to_16'
+    database = 'all_data'
+    view_index = 'year-country-state'
+    need_group = '&group=true'
+    group_level = ''
+    select_one = ''
+    select_many = ''
+
+    if ori_category == 'country' and ori_name == 'all' and au_category == 'country':
+        view_index = 'year-country-state'
+        group_level = '&group_level=2'
+
+    if ori_category == 'country' and au_category == 'state' and au_name != 'all' and au_name not in ['2001', '2006', '2011', '2016']:
+        state_name = au_name
+        view_index = 'state-country-year'
+        state_index = states_list.index(state_name)
+        if state_index == len(states_list) - 1:
+            select_many = '&startkey=["' + state_name + '"]'
+        else:
+            select_many = '&startkey=["' + state_name + '"]&endkey=["' + states_list[state_index+1] + '"]'
+
+    if ori_category == 'country' and au_category == 'sa2':
+        sa2_name = au_name
+        view_index = 'sa2-country-year'
+        sa2_index = sa2_list.index(sa2_name)
+        if sa2_index == len(sa2_list) - 1:
+            select_many = '&startkey=["' + sa2_name + '"]'
+        else:
+            select_many = '&startkey=["' + sa2_name + '"]&endkey=["' + sa2_list[sa2_index+1] + '"]'
+
+    if ori_category == 'country' and au_category == 'state' and au_name == 'all':
+        country_name = ori_name
+        view_index = 'country-state-year'
+        select_many = '&startkey=["' + country_name + '","ACT",2001]&endkey=["' + country_name + '","WA",2016]'
+
+    if ori_category == 'country' and au_category == 'state' and au_name in ['2001', '2006', '2011', '2016']:
+        view_index = 'year-country-state'
+        country_name = ori_name
+        search_year = au_name
+        select_many = '&startkey=[' + search_year + ',"' + country_name + '","ACT"]&endkey=[' + search_year + ',"' + country_name + '","WA"]'
+
+    url = 'http://wenyi:cloud@localhost:5984/' + database + '/_design/view1/_view/' + view_index + '?stable=false&update=false' + need_group + group_level + select_one + select_many
+# &startkey=[2001,"Cambodia","ACT"]&endkey=[2001,"Cambodia","WA"]
+    print(url)
+
+    response = requests.get(url, headers=headers)
+    response_json = json.loads(response.text)
+    data = response_json['rows']
+
+    existing_years = []
+    ret = {}
+    for key_value in data: # {"key":[2001,"Cambodia","ACT"],"value":221}
+        if view_index == 'state-country-year' or view_index == 'sa2-country-year' or view_index == 'country-state-year':
+            year = key_value['key'][2]
+        else:
+            year = key_value['key'][0]
+        if year not in existing_years:
+            existing_years.append(year)
+            ret[str(year)] = {'data': []}
+        entry = []
+        entry_item_1 = {}
+        if view_index == 'country-state-year':
+            entry_item_1['name'] = key_value['key'][0]
+        else:
+            entry_item_1['name'] = key_value['key'][1]
+        entry_item_1['value'] = int(key_value['value'])
+        entry.append(entry_item_1)
+        entry_item_2 = {}
+        if view_index == 'state-country-year':
+            entry_item_2['name'] = key_value['key'][0]
+        elif view_index == 'country-state-year':
+            entry_item_2['name'] = key_value['key'][1]
+        elif au_name in ['2001', '2006', '2011', '2016']:
+            entry_item_2['name'] = key_value['key'][2]
+        else:
+            entry_item_2['name'] = "Australia"
+        entry.append(entry_item_2)
+        ret[str(year)]['data'].append(entry)
+
+    return json.dumps(ret)
+
+
+
+
+@app.route('/get_initial_json')
+def get_initial_json():
+    url = 'http://wenyi:cloud@localhost:5984/test/_design/view1/_view/year-country-state?stable=false&update=false&group=true&group_level=2'
+
+    response = requests.get(url, headers=headers)
+    response_json = json.loads(response.text)
+    data = response_json['rows']
+
+    existing_years = []
+    ret = {}
+    for key_value in data:
+        year = key_value['key'][0]
+        if year not in existing_years:
+            existing_years.append(year)
+            ret[str(year)] = {'data': []}
+        entry = []
+        entry_item_1 = {}
+        entry_item_1['name'] = key_value['key'][1]
+        entry_item_1['value'] = int(key_value['value'])
+        entry.append(entry_item_1)
+        entry_item_2 = {}
+        entry_item_2['name'] = "Australia"
+        entry.append(entry_item_2)
+        ret[str(year)]['data'].append(entry)
+
+    return json.dumps(ret)
+
+
+
+@app.route('/get_all_countries_to_one_state')
+def get_all_countries_to_one_state():
+    url = 'http://wenyi:cloud@localhost:5984/test/_design/view1/_view/state-country-year?stable=false&update=false&group=true&startkey=["VIC"]&endkey=["WA"]'
+
+    response = requests.get(url, headers=headers)
+    response_json = json.loads(response.text)
+    data = response_json['rows']
+
+    existing_years = []
+    ret = {}
+    for key_value in data:
+        year = key_value['key'][2]
+        if year not in existing_years:
+            existing_years.append(year)
+            ret[str(year)] = {'data': []}
+        entry = []
+        entry_item_1 = {}
+        entry_item_1['name'] = key_value['key'][1]
+        entry_item_1['value'] = int(key_value['value'])
+        entry.append(entry_item_1)
+        entry_item_2 = {}
+        entry_item_2['name'] = key_value['key'][0]
+        entry.append(entry_item_2)
+        ret[str(year)]['data'].append(entry)
+
+    print(ret)
+    return json.dumps(ret)
+
+
+
 
 
 @app.route('/getjsonData')
@@ -36,6 +213,33 @@ def getjsonData():
     data = {'name': 'cats', 'attribute': 'cute'}
     data = json.dumps(data)
     return data
+
+# 可以用python访问，但是查询很不灵活，特别是key不能用数组形式，决定了我不能用这个
+@app.route('/get_couchdb_data')
+def get_couchdb_data():
+    server = couchdb.Server('http://wenyi:cloud@localhost:5984/')
+    db = server['test']
+
+    #state-to-continent
+    #country-to-state
+    count = 0
+    dict = {}
+    dict['data'] = []
+    for item in db.view('view1/country-to-state', group=True, group_level=1, stable=False, update=False, key="['Afghanistan']"):
+        if count < 2:
+            print(item.key, item.value)
+            entry = []
+            entry_item_1 = {}
+            entry_item_1['name'] = item.key[0]
+            entry_item_1['value'] = int(item.value)
+            entry.append(entry_item_1)
+            entry_item_2 = {}
+            entry_item_2['name'] = "Australia"
+            entry.append(entry_item_2)
+            dict['data'].append(entry)
+            count += 1
+        else:
+            break
 
 
 if __name__ == '__main__':
---
+++ website/app/static/js/myCartogram.js
@@ -0,0 +1,342 @@
+var stateDict = {
+  'NSW': [1, 'New South Wales'],
+  'VIC': [2, 'Victoria'],
+  'QLD': [3, 'Queensland'],
+  'SA': [4, 'South Australia'],
+  'WA': [5, 'Western Australia'],
+  'TAS': [6, 'Tasmania'],
+  'NT': [7, 'Northern Territory'],
+  'ACT': [8, 'Australian Capital Territory']
+};
+
+var features, path;
+var margin = {
+    top: 10,
+    bottom: 10,
+    left: 10,
+    right: 10
+  },
+  width = parseInt(d3.select('#chart').style('width')),
+  width = width - margin.left - margin.right;
+
+// I need to size this within reason.
+// width is not ever going to be bigger than 600 px
+// If it's too big, I'm having a hard time figuring out scale and translate
+
+if (width > 600) {
+  width = 600;
+}
+
+var mapRatio = 1.2,
+  height = width * mapRatio;
+
+var tooltip_div = d3.select("body").append("div")
+  .attr("class", "tooltip")
+  .style("opacity", 0);
+
+var svg = d3.select("#chart").append("svg")
+  .attr("width", width - margin.left - margin.right)
+  .attr("height", height - margin.top - margin.bottom);
+
+var counties = svg.append("g")
+  .attr("id", "counties")
+  .selectAll("path"); // TODO
+
+
+// works for v3. don't need to rotate by using mercator projection
+var projection = d3.geo.mercator()
+  .scale(width)
+  .translate([-1100, -50]);
+
+var topology,
+  geometries,
+  carto_features;
+
+var number_formatter = d3.format("0,000");
+
+var pop_data = d3.map();
+
+// var carto;
+
+var carto = d3.cartogram()
+  .projection(projection)
+  .properties(function(d) {
+    return d.properties;
+  });
+
+
+// var statesData2016 = {
+//     "code": "success",
+//     "data": [{
+//         "STATE_CODE": "STATE_CODE",
+//         "STATE_NAME": "STATE_NAME",
+//         "POP": "POP"
+//     }, {
+//         "STATE_CODE": "1",
+//         "STATE_NAME": "New South Wales",
+//         "POP": "100"
+//     }, {
+//         "STATE_CODE": "2",
+//         "STATE_NAME": "Victoria",
+//         "POP": "200"
+//     }, {
+//         "STATE_CODE": "3",
+//         "STATE_NAME": "Queensland",
+//         "POP": "300"
+//     }, {
+//         "STATE_CODE": "4",
+//         "STATE_NAME": "South Australia",
+//         "POP": "400"
+//     }, {
+//         "STATE_CODE": "5",
+//         "STATE_NAME": "Western Australia",
+//         "POP": "500"
+//     }, {
+//         "STATE_CODE": "6",
+//         "STATE_NAME": "Tasmania",
+//         "POP": "600"
+//     }, {
+//         "STATE_CODE": "7",
+//         "STATE_NAME": "Northern Territory",
+//         "POP": "700"
+//     }, {
+//         "STATE_CODE": "8",
+//         "STATE_NAME": "Australian Capital Territory",
+//         "POP": "800"
+//     }]
+// }
+
+
+// d3.json("src/data/test_data_2016.json", function(data) {
+//   console.log(data.data);
+
+
+  // statesData2016.data.forEach(function(d) {
+  //   // console.log(d);
+  //   pop_data.set(d.STATE_CODE, [d.STATE_NAME, d.POP]);
+  // });
+// })
+
+
+
+
+
+// topology = topoStates;
+// geometries = topology.objects['collection'].geometries;
+
+// var features = carto.features(topology, geometries);
+// // 定义地理路径生成器
+// var path = d3.geo.path()
+//   .projection(projection);
+
+// console.log(topology);
+// console.log(geometries);
+// console.log(features);
+
+//add this to make up for counties with a space in the name: .replace(/\s+/g, '')
+// counties = counties.data(features)
+//   .enter()
+//   .append("path")
+//   .attr("class", "county")
+//   .attr("id", function(d) {
+//     return d.properties.name.replace(/\s+/g, '');
+//   })
+//   .attr("fill", "white")
+//   .attr("d", path)
+//   .attr("stroke", "black")
+//   .attr("stroke-width", "0.3px")
+//   .on("mouseover", function(d) {
+//     // console.log(d);
+//     // console.log(pop_data.get(d.properties['GEOID10']));
+
+//     d3.selectAll("#" + d.properties.name.replace(/\s+/g, '')).style("fill", "#99CC33");
+//     tooltip_div.transition()
+//       .duration(200)
+//       .style("opacity", 0.9)
+//       .style("border", "1px black solid")
+//     tooltip_div.html(d.properties.name + "<br>" +
+//         number_formatter(pop_data.get(d.properties['STATE_CODE'])[1]))
+//       .style("left", (d3.event.pageX) + "px")
+//       .style("top", (d3.event.pageY - 28) + "px")
+//       .style("height", "50px");
+//   })
+//   .on("mouseout", function(d) {
+//     d3.selectAll("#" + d.properties.name.replace(/\s+/g, '')).style("fill", "white");
+//     tooltip_div.transition()
+//       .duration(500)
+//       .style("opacity", 0);
+//   });
+
+
+function doNormal() {
+  d3.select("#click_to_normal").text("thinking...");
+
+  var features = carto.features(topology, geometries),
+    path = d3.geo.path()
+    .projection(projection);
+
+  counties.data(features)
+    .transition()
+    .duration(1500)
+    .each("end", function() {
+      d3.select("#click_to_normal").text("View Normal")
+    })
+    .attr("d", path);
+};
+
+function doUpdate() {
+  d3.select("#click_to_run").text("thinking...");
+  console.log('mark 111111');
+  carto.value(function(d) {
+    console.log(+pop_data.get(d.properties['STATE_CODE'])[1])
+    return +pop_data.get(d.properties['STATE_CODE'])[1];
+  });
+
+console.log('mark 222222');
+  if (carto_features == undefined) {
+    carto_features = carto(topology, geometries).features;
+  }
+
+console.log('mark 333333');
+  counties.data(carto_features)
+    .text(function(d) {
+      return d.properties.STATE_CODE;
+    })
+console.log('mark 444444');
+
+  counties.transition()
+    .duration(1500)
+    .each("end", function() {
+      d3.select("#click_to_run").text("View by Population");
+    })
+    .attr("d", carto.path);
+   console.log('mark 555555');
+}
+
+$("#choose-area-button").click(function(event) {
+  $("#choose-area-panel-cartogram").css("visibility", "visible");
+  $("#choose-area-button").css("visibility", "hidden");
+});
+
+
+
+var firstTime = true;
+
+$("#choose-panel-confirm").click(function(event) {
+  console.log("click confirm");
+  formData = $('form').serializeArray();
+  console.log(formData);
+  console.log(JSON.stringify(formData, null, 4));
+  $("#choose-area-panel-cartogram").css("visibility", "hidden");
+  $("#choose-area-button").css("visibility", "visible");
+
+  originCategory = 'country'
+  originName = formData[1]['value'];
+  auCategory = 'state';
+  auName = 'all';
+  auYear = formData[4]['value']
+  auName = auYear
+
+  console.log(originCategory, originName, auCategory, auName, auYear);
+
+  var url = '/getjson/' + originCategory + '/' + originName + '/' + auCategory + '/' + auName;
+
+  $.getJSON(url, function(result) {
+    tbp = result;
+    console.log(tbp);
+    var dataArr = Object.values(result)[0]['data'];
+    console.log(dataArr);
+    var statesDataThisYear = {
+      'data': []
+    }
+    for (var i = 0; i < dataArr.length; i++) {
+      var dataInOneState = dataArr[i];
+      // console.log(dataInOneState);
+      var abbr = dataInOneState[1]['name'];
+      var tempDict = {};
+      tempDict['STATE_CODE'] = stateDict[abbr][0];
+      tempDict['STATE_NAME'] = stateDict[abbr][1];
+      tempDict['POP'] = dataInOneState[0]['value'];
+      statesDataThisYear['data'].push(tempDict);
+    }
+    console.log(statesDataThisYear);
+
+pop_data = d3.map();
+    statesDataThisYear.data.forEach(function(d) {
+      // console.log(d);
+      pop_data.set(d.STATE_CODE, [d.STATE_NAME, d.POP]);
+    });
+
+    if (firstTime) {
+      // firstTime = false;
+
+    topology = topoStates;
+    geometries = topology.objects['collection'].geometries;
+
+    features = carto.features(topology, geometries);
+    // 定义地理路径生成器
+    path = d3.geo.path()
+      .projection(projection);
+
+    // console.log(topology);
+    // console.log(geometries);
+    // console.log(features);
+
+    //add this to make up for counties with a space in the name: .replace(/\s+/g, '')
+    counties = counties.data(features)
+      .enter()
+      .append("path")
+      .attr("class", "county")
+      .attr("id", function(d) {
+        return d.properties.name.replace(/\s+/g, '');
+      })
+      .attr("fill", "white")
+      .attr("d", path)
+      .attr("stroke", "black")
+      .attr("stroke-width", "0.3px")
+      .on("mouseover", function(d) {
+        // console.log(d);
+        // console.log(pop_data.get(d.properties['GEOID10']));
+
+        d3.selectAll("#" + d.properties.name.replace(/\s+/g, '')).style("fill", "#99CC33");
+        tooltip_div.transition()
+          .duration(200)
+          .style("opacity", 0.9)
+          .style("border", "1px black solid")
+        tooltip_div.html(d.properties.name + "<br>" +
+            number_formatter(pop_data.get(d.properties['STATE_CODE'])[1]))
+          .style("left", (d3.event.pageX) + "px")
+          .style("top", (d3.event.pageY - 28) + "px")
+          .style("height", "50px");
+      })
+      .on("mouseout", function(d) {
+        d3.selectAll("#" + d.properties.name.replace(/\s+/g, '')).style("fill", "white");
+        tooltip_div.transition()
+          .duration(500)
+          .style("opacity", 0);
+      });
+    }
+  });
+});
+
+
+
+var tbp;
+
+$("#choose-panel-close").click(function(event) {
+  console.log("click close");
+  $("#choose-area-panel-cartogram").css("visibility", "hidden");
+  $("#choose-area-button").css("visibility", "visible");
+});
+
+$("#close").click(function(event) {
+  console.log("click close");
+  $("#choose-area-panel-cartogram").css("visibility", "hidden");
+  $("#choose-area-button").css("visibility", "visible");
+});
+
+$("#refresh").click(function() {
+  window.location.reload();
+})
+
+
